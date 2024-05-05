9d93f5cdd1084c980e008d8401beda0469ffb549
seanmcilvane <seanmcilvane@gmail.com>
2024-05-05 16:29:24 -0700

    - Change from Flask to FASTAPI for better performance and asynchonous api - update requirements.txt - predict api works for a single data point

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,48 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from pyspark.sql import SparkSession
 from pyspark.ml import PipelineModel
+from pyspark.sql.types import StructType, StructField, FloatType
+import typing
+from typing import List
 import mlflow.pyfunc
+import numpy as np
+import pandas as pd
 
-app = Flask(__name__)
+app = FastAPI()
 
 # Load the model
 spark = SparkSession.builder.appName("Model Serving App").getOrCreate()
 # model = PipelineModel.load("model")
 model = mlflow.pyfunc.load_model("model")
 
-@app.route('/', methods=['GET'])
-def get():
+class PredictionInput(BaseModel):
+    data: List[List[float]]
+
+
+@app.get("/")
+async def get():
     return "woo"
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Expect JSON input containing the new data for prediction
-    data = request.json
-    df = spark.createDataFrame([data])  # Convert JSON to DataFrame
-    prediction = model.transform(df)
-    # Convert prediction to JSON or a simple response
-    return jsonify(prediction.select('prediction').collect())
+@app.post("/predict/")
+async def predict(input: PredictionInput):
+    # # Expect JSON input containing the new data for prediction
+    # data = request.json
+    try:
+        schema = StructType([
+            StructField("sepal_length", FloatType(), True),
+            StructField("sepal_width", FloatType(), True),
+            StructField("petal_length", FloatType(), True),
+            StructField("petal_width", FloatType(), True)
+        ])
+        #data = np.ndarray([5.1, 3.5, 1.4, 0.2])
+        # data_df = spark.createDataFrame([tuple(input.data)], schema=schema)
+        data_df = pd.DataFrame([input.data], columns=["sepal_length", "sepal_width", "petal_length", "petal_width"])
+        predictions = model.predict(data_df)
+        return {"prediction": predictions}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-if __name__ == '__main__':
-    # app.run(host='172.17.0.1')
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
